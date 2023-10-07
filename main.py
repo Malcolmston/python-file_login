bf84a9df214649b77abd674410fa397d0ece4c2b
alcolmston <mstone@crefeld.org>
2023-10-07 12:38:03 -0400

    Log in form
    this is a basic log in form. this page has 2 inputs for a user to input a value to. There are default values for testing.

--- main.py
+++ main.py
@@ -9,7 +9,35 @@ window = Tk()
 
 window.geometry("600x600")
 
-frame = Frame(window)
+log_in_frame = Frame(window)
+
+
+
+Label(log_in_frame , text="Log in", width=13,font=("arial",20)).grid(column=3, row=1)
+
+
+Label(log_in_frame , text="Username").grid(column=2, row=2)
+username = Entry(log_in_frame, width=26)
+username.insert(0, "malcolm")
+
+Label(log_in_frame , text="Password").grid(column=2, row=3)
+password = Entry(log_in_frame , show="*", width=26)
+password.insert(0, "Malcolmstone18")
+
+
+button = Button(log_in_frame, text= "enter")
+
+
+username.grid(column=3, row=2)
+password.grid(column=3, row=3)
+
+button.bind("<Button-1>", print)
+
+button.grid(column=3, row=4)
+
+log_in_frame.place(x=5, y=0)
+
+
 
 
 window.mainloop()
