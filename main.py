7c03fab8291dbb50ed491f204d012ce395dabf95
alcolmston <mstone@crefeld.org>
2023-10-07 12:32:56 -0400

    Update main.py
    basic 600 * 600 px page set up

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
 from tkinter import simpledialog
+
+
+window = Tk()
+
+window.geometry("600x600")
+
+frame = Frame(window)
+
+
+window.mainloop()
