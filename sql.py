092674c6bd8a0a07c0722f9a0ed61b70c9c45aeb
alcolmston <mstone@crefeld.org>
2023-10-07 20:01:40 -0400

    replace all function
    This function usees regular expressions to replace all the intended characters in a string. Like the Javascript version, it works on a similar property

--- sql.py
+++ sql.py
@@ -1,3 +1,18 @@
 import sqlite3
 import numpy
 import re
+
+
+# replace all the charicters in a string with a new charicter
+def replace_all(sub, new_line, string):
+    org = string  # unmadified string for forever problom
+    i = 0
+    while (sub in string):
+        string = string.replace(sub, new_line)
+
+        if i >= len(org):
+            return string
+
+        i += 1
+
+    return string
