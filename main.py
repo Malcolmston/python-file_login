999077a1046b8c01f575d855504edd0f97df4750
alcolmston <mstone@crefeld.org>
2023-10-09 09:18:59 -0400

    test hash

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ sql.run(sql.create_table("files", ','.join(file_table)))
 
 
 sql.run( 
-    sql.insert("users", "dip_name, username, password, email, type, deleted", "'a','a','a','a','basic', current_date")
+    sql.insert("users", "dip_name, username, password, email, type, deleted", "'a','a',hash('a'),'a','basic', current_date")
 )
 
 
