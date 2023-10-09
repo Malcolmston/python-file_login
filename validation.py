c484795f451355d0ee8c2e169138aabe52a72a77
alcolmston <mstone@crefeld.org>
2023-10-09 09:03:42 -0400

    Update validation.py
    adds the ability to hide a password, and set a salt if need be

--- validation.py
+++ validation.py
@@ -1,1 +1,17 @@
 import hashlib
+
+def hide_password(password = None, salt = ''):
+    '''
+    using hashlib this function will hash a password
+
+    Inputs:
+        password (str | None): a user's password'
+        salt (str): for admin users to be able to salt their pasword
+    Returns:
+        hashed-password (str | None): a user's hashed password'
+
+    '''
+        # Encoding the password
+    hashed = hashlib.sha256((salt + password).encode())
+
+    return str( hashed.hexdigest() )
