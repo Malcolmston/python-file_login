0032479ab943580e160a8fbc1c314917cf7a36d8
alcolmston <mstone@crefeld.org>
2023-10-09 09:06:37 -0400

    Email + Password validation
    is_valid_Email validates a users email
    is_valid_Password validates a users password

--- validation.py
+++ validation.py
@@ -15,3 +15,17 @@ def hide_password(password = None, salt = ''):
     hashed = hashlib.sha256((salt + password).encode())
 
     return str( hashed.hexdigest() )
+
+
+def is_valid_Email(email = None):
+    # thnks to https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/# for reqex
+    regex = r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+
+    return bool(re.fullmatch(regex, email))
+
+
+def is_valid_Password(password = None):
+    regex = r"([A-Z]{1,9})+([a-z]{7,19})+([0-9]{2,7})"
+
+    return bool(re.fullmatch(regex, password))
+
